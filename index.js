eb9a669561a383c31ce8eef3f6a15f2a27377a76
Edvold <56292479+Edvold@users.noreply.github.com>
2021-02-11 08:36:44 +0100

    Add files via upload
    Start of the project

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '!';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Schoolbot is online');
+});
+
+
+
+
+client.login('ODAzNTM3NTUwMTczMjc0MTUz.YA_OqA.05MAfZnZVuNYV7UKR3uQbNciEQY');
