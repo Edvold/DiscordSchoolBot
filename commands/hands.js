bb53fbc175bb7b3722e584bc62b4ffbbb6a7a488
Edvold <56292479+Edvold@users.noreply.github.com>
2021-02-11 09:36:57 +0100

    Added a command handler

--- commands/hands.js
+++ commands/hands.js
@@ -1,5 +1,15 @@
 module.exports = {
     name: 'hands',
     description: "Raise your hands!",
-    execute()
+    execute(message, args) {
+        switch(args) {
+            case 'raise':
+                message.channel.send('You raised your hand');
+                return;
+            case 'lower':
+                message.channel.send('You lowered your hand');
+                return;
+        }
+        
+    }
 }
--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-
     client.commands.set(command.name, command);
 }
 
@@ -20,6 +19,19 @@ client.once('ready', () => {
     console.log('Schoolbot is online');
 });
 
+client.on('message', message =>{
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+
+    const args = message.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+
+    if (command === 'hands') {
+        client.commands.get('hands').execute(message, args);
+    }
+
+})
 
 
 
