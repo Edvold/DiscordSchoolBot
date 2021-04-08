b955e18a21c916650222bf6ac862dd59a25a94f9
Edvold <56292479+Edvold@users.noreply.github.com>
2021-04-08 08:48:36 +0200

    added ability to change config.json from discord

---
+++ commands/config.js
@@ -0,0 +1,35 @@
+fs = require('fs');
+
+module.exports = {
+    name: 'config',
+    description: 'change config settings',
+    commands: ['config', 'cfg', 'c', 'settings'],
+    expectedArgs: '[name of config settings] [new value]',
+    minArgs: 2,
+    maxArgs: 2,
+    callback: (message, args, text) => {
+        config.edit(message, args);
+    }
+}
+
+class Config {
+    constructor() {
+        this.file = 'config.json';
+    }
+
+    edit(message, args) {
+        let json = JSON.parse(fs.readFileSync(this.file.toString()));
+
+        for (const key in json) {
+            if (args[0] === key) {
+                json[key] = args[1];
+            }
+        }
+
+        
+
+        fs.writeFileSync(this.file, JSON.stringify(json));
+    }
+}
+
+const config = new Config();
